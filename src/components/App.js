65d0e4c5afe3df7b78fe1f0fb6c31f7614256e67
cmulliss <cmulliss@gmail.com>
2020-01-22 18:11:54 +0000

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './App.css'
+import './homepage.styles.scss'
+import HomePage from './homepage'
+
+function App () {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
