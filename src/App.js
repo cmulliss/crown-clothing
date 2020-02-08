6c3b0e41a4daeb55b23b94525f87c1f5cf2d3f50
cmulliss <cmulliss@gmail.com>
2020-02-08 17:03:35 +0000

    header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
 import './App.css'
 import './pages/homepage/homepage.styles.scss'
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header'
 
-const App = () => {
+function App () {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   )
 }
--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,24 +1,28 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
+
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => {
-  return (
-    <div className={`${size}  menu-item`}>
-      <div
-        className="background-image"
-        style={{
-          backgroundImage: `url(${imageUrl})`
-        }}
-      />
-      <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">SHOP NOW</span>
-      </div>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size}  menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
     </div>
-  )
-}
+  </div>
+)
 
-export default MenuItem
+export default withRouter(MenuItem)
 
 // can destructure our title off the props
 // same as props.title, except pulling that title value off of our props, then setting it the this title value inside our fn, so we can use it whenever we want
+// withRouter is a HOC, essentially a fn that takes a component as an arg adn returns a modified component, this returns a MenuItem with access to the location, match and history props
